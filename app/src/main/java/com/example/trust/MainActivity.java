dae8342c29a133428bb1a5c08b27bd226fd16b6c
Hangeulkim <ckswls89@naver.com>
2019-11-10 14:42:33 +0900

    init

---
+++ app/src/main/java/com/example/trust/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.trust;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout Select_More_Layout;
+    LinearLayout Select_Start_Layout;
+    LinearLayout Start_First_Layout;
+
+    Button Start_More;
+    Button Start_Start;
+
+    public void Click_Select_Log(View view){
+
+    }
+
+    public void Click_Select_Setting(View view){
+
+    }
+
+    public void Click_Select_Makers(View view){
+
+    }
+
+    public void Click_More_Quit(View view){
+        Select_More_Layout.setVisibility(View.INVISIBLE);
+    }
+
+
+    public void Click_Start_More(View view){
+        Start_First_Layout.setVisibility(View.INVISIBLE);
+        Select_More_Layout.setVisibility(View.VISIBLE);
+    }
+
+    public void Click_Start_Start(View view){
+        Start_More.setVisibility(View.VISIBLE);
+        Start_Start.setVisibility(View.INVISIBLE);
+    }
+
+    public void Click_Start_Timer(View view){
+
+    }
+
+    public void Click_Start_Select(View view){
+
+    }
+
+    public void Click_Start_Fast(View view){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Select_More_Layout=(LinearLayout)findViewById(R.id.Select_More);
+        Select_Start_Layout=(LinearLayout)findViewById(R.id.Select_Start);
+        Start_First_Layout=(LinearLayout)findViewById(R.id.Start_First);
+
+        Start_More=(Button)findViewById(R.id.Start_More);
+        Start_Start=(Button)findViewById(R.id.Start_Start);
+    }
+}
