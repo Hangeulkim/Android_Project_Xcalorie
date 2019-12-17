5e716679e6d260701e044a70abec38cc41005815
chongin <chongin12@naver.com>
2019-12-17 23:13:04 +0900

    pull

--- app/src/main/java/com/example/trust/MainActivity.java
+++ app/src/main/java/com/example/trust/MainActivity.java
@@ -33,8 +33,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.text.SimpleDateFormat;
